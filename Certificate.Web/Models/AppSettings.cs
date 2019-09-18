7039ab47edd15dd0ba813cdcdc512bd89070dbf8
Joseph Todd <joseph.todd@ucdenver.edu>
2019-09-18 08:49:11 -0600

    update gitignore

---
+++ Certificate.Web/Models/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Certificate.Web.Models
+{
+    public class AppSettings
+    {
+        public string BaseUrl { get; set; }
+        public string CanvasBaseUrl { get; set; }
+        public string CanvasApiKey { get; set; }
+    }
+}
